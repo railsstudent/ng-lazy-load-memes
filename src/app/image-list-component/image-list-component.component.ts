5184187d20c06a9f2b78481f75234eae247cf260
Connie Leung <cleung334@gmail.com>
2018-07-08 14:43:57 +0800

    Move constant data to util.js.  Specify observer type to enforce static type checking

--- src/app/image-list-component/image-list-component.component.ts
+++ src/app/image-list-component/image-list-component.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, AfterViewInit, QueryList, ViewChildren, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  QueryList,
+  ViewChildren,
+  ElementRef,
+  Renderer2
+} from '@angular/core';
 import { WindowRef } from '../WindowRef';
-import { fetchImage, lazyLoadImage } from './util';
-
-const option = {
-  root: null,
-  rootMargin: '0px',
-  threshold: [0, 0.25]
-};
+import {
+  fetchImage,
+  lazyLoadImage,
+  option,
+  MEMES
+} from './util';
 
 @Component({
   selector: 'app-image-list-component',
@@ -15,56 +22,8 @@ const option = {
 })
 export class ImageListComponentComponent implements OnInit, AfterViewInit {
 
-  observer: any;
-  urls = [
-    { placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-01-720x539.jpg'
-    },
-    { placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-02-720x932.jpg'
-    },
-    { placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-03-720x697.jpg'
-    },
-    { placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-04-720x540.jpg'
-    },
-    { placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-05-720x530.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-06-720x504.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-07-720x950.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-08-720x519.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-09-720x662.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-10-720x885.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-11-720x540.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-12-720x463.jpg'
-    },
-    {
-      placeholder: 'https://via.placeholder.com/600x480',
-      actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-17-720x520.jpg'
-    }
-  ];
+  observer: IntersectionObserver;
+  urls = MEMES;
 
   @ViewChildren('image')
   images: QueryList<ElementRef>;
@@ -87,13 +46,14 @@ export class ImageListComponentComponent implements OnInit, AfterViewInit {
     });
   }
 
-  observerCallback(entries: IntersectionObserverEntry[], observer) {
+  observerCallback(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
     entries.forEach((entry) => {
       const img = entry.target as HTMLImageElement;
       if (entry.intersectionRatio > 0) {
         this.renderer.removeClass(img, 'invisible');
       } else {
         this.renderer.addClass(img, 'invisible');
+        console.log(`Hide image ${img.id} to reduce memory usage`);
       }
       if (entry.intersectionRatio > 0.25) {
         if (img.dataset.src) {
--- src/app/image-list-component/util.ts
+++ src/app/image-list-component/util.ts
@@ -15,3 +15,59 @@ export const lazyLoadImage = (img: HTMLImageElement) => {
   return fetchImage(img.dataset.src)
     .then((url: string) => img.src = url);
 };
+
+export const option = {
+  root: null,
+  rootMargin: '0px',
+  threshold: [0, 0.25]
+};
+
+export const MEMES = [
+  { placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-01-720x539.jpg'
+  },
+  { placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-02-720x932.jpg'
+  },
+  { placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-03-720x697.jpg'
+  },
+  { placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-04-720x540.jpg'
+  },
+  { placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-05-720x530.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-06-720x504.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-07-720x950.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-08-720x519.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-09-720x662.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-10-720x885.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-11-720x540.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-12-720x463.jpg'
+  },
+  {
+    placeholder: 'https://via.placeholder.com/600x480',
+    actual: 'https://winkgo.com/wp-content/uploads/2018/05/55-Funniest-Cat-Memes-Ever-17-720x520.jpg'
+  }
+];
